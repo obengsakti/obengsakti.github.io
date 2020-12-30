438069c3c93d36ea607aef4030c10eca04806f5b
obengsakti <obengsakti@github.io>
2020-12-31 02:53:01 +0700

    kerangka

---
+++ js/navchat.js
@@ -0,0 +1,18 @@
+ <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
+
+  
+      <script id="rendered-js" >
+/*     NOTES:
+ * [1] Number of list-items you want
+ * [2] Size of closed menu "icon"
+ * [3] Size of your expanded menu
+ * [4] Size of the small units
+ * [5] Used to transform the origin of the rotation on the list-itmes to the middle of the menu so they spin out in a centered circle. The same number also offsets position of the list items so they don't sit right on the edge of the actual <ul>, that way there's padding and the menu won't close down if the user moves their cursor a bit too far.
+ * [6] Used for the degrees of rotation between each list item, keeping them all evenly spaced. 360 degrees in a circle divided by the number of list items you have
+ */
+//# sourceURL=pen.js
+    </script>
+
+  
+
+  <script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeRefreshCSS-bb9a2ba1f03f6147732cb3cd52ac86c6b24524aa87a05ed0b726f11e46d7e277.js"></script>
